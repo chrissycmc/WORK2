0e4270900408c6a73aa1054653ee6df1fff00bd6
chrissycmc <chrissycmc0@gmail.com>
2025-03-05 14:44:41 +0300

    commit#2
    In the new version on line 7 to 10 I have a constructor

--- WORK2/myModule2/src/package2/Dept.java
+++ WORK2/myModule2/src/package2/Dept.java
@@ -3,4 +3,10 @@ package package2;
 public class Dept {
  int DeptCode;
  String DeptName;
+
+ public Dept(int DeptCode,String DeptName){
+     this.DeptCode= DeptCode;
+     this.DeptName=DeptName;
+ }
+
 }
