ea809d45a7cef24f2726c98cd3321ac6e2991c4d
chrissycmc <chrissycmc0@gmail.com>
2025-03-05 15:39:15 +0300

    commit#2
    In the new version on line 4 to 6 I have an instance of a class

--- WORK2/src/Main.java
+++ WORK2/src/Main.java
@@ -1,8 +1,12 @@
 import package1.*;
 import package2.*;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Student student=new Student("Christina",813);
+        Dept dept=new Dept(1990,"COMPUTER SCIENCE");
 
+        dept.displayDeptInfo();
 
     }
+
 }
