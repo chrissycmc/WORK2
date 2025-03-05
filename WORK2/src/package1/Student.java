700d803588efc6d0f35fb15fbefd569c2a7f7d61
chrissycmc <chrissycmc0@gmail.com>
2025-03-05 14:13:28 +0300

    commit#1
    In the new version on line 7 up to 10 I have created a method to call the student variables

--- WORK2/src/package1/Student.java
+++ WORK2/src/package1/Student.java
@@ -3,4 +3,9 @@ package package1;
 public class Student {
     String Name;
     private int StudentID;
+
+    public Student(String Name,int StudentID){
+        this.Name=Name;
+        this.StudentID=StudentID;
+    }
 }
